4ea83b748d935bff7a2ba8e81d98d9a6ae60b3a6
Giulia Silvestrini <silvergiuly@hotmail.it>
2023-12-01 16:16:50 +0100

    change switch

--- src/Main.java
+++ src/Main.java
@@ -16,15 +16,14 @@ public class Main {
         for (int i = 0; i < 5; i++) {
             System.out.println("Cosa vuoi creare un video , un' immagine o un audio?");
             String tipo = scanner.nextLine().toLowerCase();
-            System.out.println("Inserisci il titolo dell'elemento " + (i +1 ) + ":");
+            System.out.println("Inserisci il titolo dell'elemento " + (i + 1 ));
             String titolo = scanner.nextLine();
 
-            switch (tipo.toLowerCase()) {
-                case "audio":
-                    System.out.println("Inserisci il volume da uno a 5 ! :");
+            switch (tipo.toLowerCase() ) {
+                case "audio" -> {  System.out.println("Inserisci il volume da uno a 5 ! :");
                     String volumeInput = scanner.nextLine();
                     if (volumeInput.length() > 5) {
-                        System.out.println("Il volume deve avere al massimo lunghezza 5.");
+                        System.out.println("Il volume deve avere al massimo 5 punti esclamativi.");
                         i--;
                     }
                     char[] volumeValue = volumeInput.toCharArray();
@@ -32,14 +31,15 @@ public class Main {
                     System.out.println("Inserisci la durata:");
                     int durata = scanner.nextInt();
                     scanner.nextLine();
-                    elementi[i] = new Audio(durata, PlayAndVolume.volumeValue);
-                    break;
+                    elementi[i] = new Audio(durata, PlayAndVolume.volumeValue);}
 
-                case "video":
+
+
+                case "video"-> {
                     System.out.println("Inserisci il volume inserendo il carattere ! da una a 5 volte:");
                     String volumeVideoInput = scanner.nextLine();
                     if (volumeVideoInput.length() > 5) {
-                        System.out.println("Il volume deve avere al massimo  5 !");
+                        System.out.println("Il volume deve avere al massimo 5 punti esclamativi.");
                         i--;
                         break;
                     }
@@ -49,35 +49,35 @@ public class Main {
                     int durataVideo = scanner.nextInt();
                     scanner.nextLine();
 
-                    System.out.println("Inserisci la luminosità da uno a 5 * ");
+                    System.out.println("Inserisci l' asterisco inserendo il carattere * da una a 5 volte:");
                     String lumVideoInput = scanner.nextLine();
                     if (lumVideoInput.length() > 5) {
-                        System.out.println("La luminosità deve avere al massimo 5 *");
+                        System.out.println("La luminosità deve avere al massimo 5 asterischi");
                         i--;
-                        break;
+
                     }
                     char[] lumVideoValue = lumVideoInput.toCharArray();
 
                     elementi[i] = new Video(durataVideo, PlayAndVolume.volumeValue, Luminosità.luminositàValue);
-                    break;
+                }
 
-                case "immagine":
-                    System.out.println("Inserisci la luminosità da uno a 5 * ");
+                case "immagine"-> {
+                    System.out.println("Inserisci l' asterisco inserendo il carattere * da una a 5 volte:");
                     String lumInput = scanner.nextLine();
                     if (lumInput.length() > 5) {
-                        System.out.println("La luminosità deve avere al massimo 5 *.");
+                        System.out.println("La luminosità deve avere al massimo 5 asterischi");
                         i--;
                         break;
                     }
                     char[] lumValue = lumInput.toCharArray();
 
                     elementi[i] = new Immagine(Luminosità.luminositàValue);
-                    break;
+                }
 
-                default:
+                default -> {
                     System.out.println("Tipo non esistente");
-                    i--;
-                    break;
+                    i--;}
+
             }
         }
 
